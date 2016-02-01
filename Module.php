e8b6d05c7b808ead62fac5ba730d00ce56c3a73d
Ibrahim Azhar Armar <azhar@iarmar.com>
2016-02-01 15:46:04 +0530

    Initial Commit

---
+++ Module.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Zf2 Module Class
+ *
+ * @author Ibrahim Azhar <azhar@iarmar.com>
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+namespace Oml\Zf2ApiService;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src',
+                ),
+            ),
+        );
+    }
+}
